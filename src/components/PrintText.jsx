74b1b4bcbbb4fd28446db9190b739fecb0131b98
StivenLaiton <stiven.laitonjimenez@gmail.com>
2022-01-04 02:28:19 -0500

    tercer commit

--- src/components/PrintText.jsx
+++ src/components/PrintText.jsx
@@ -1,17 +1,18 @@
-export default function PrintText( {resultado} ) {
+export default function PrintText( resultado ) {
 
     
-  
+    resultado = resultado.datos
     console.log(resultado)
+    
     
 
     if(resultado.state === true){
-        const nacionalidades = resultado.datos.nacionalidades
+        const nacionalidades = resultado.nacionalidades
         return (
       <div className="print-text">
         <h2>Estos son los siguientes datos para: {resultado.nombre}</h2>
-        <h3 >Su edad es: {resultado.datos.edad}</h3>
-        <h3 >Su genero es: {resultado.datos.genero}</h3>
+        <h3 >Su edad es: {resultado.edad}</h3>
+        <h3 >Su genero es: {resultado.genero}</h3>
         <ol >y sus nacionalidades pueden ser: {nacionalidades.map((nacionalidad) => {
             return <li  key={nacionalidades.lenght}>  {nacionalidad.country_id} con una probabilidad del: {nacionalidad.probability}</li>
         })}</ol>
--- src/components/Recuadro.jsx
+++ src/components/Recuadro.jsx
@@ -3,60 +3,36 @@ import PrintText from "./PrintText";
 import useService from "../hooks/UseService";
  function Recuadro() {
     const [text, setText] = useState("")
-    const [resultado, setResultado] = useState()
-    let simulacionderequest = { 
-        datos: {
-        nacionalidades: [
-            {
-                country_id: "Col",
-                probability: 0.002254
-            },
-            {  
-                country_id: "arg",
-                probability: 0.902254
-            } 
- 
-        ],
-        edad: 24,   
-        genero: "male",
-        }         
-    }
-    
-
-    let  datos1 = useService(`https://api.nationalize.io/?name=${text}`)
-    let  datos2 = useService(`https://api.agify.io/?name=${text}`)
-    let  datos3 = useService(`https://api.genderize.io?name=${text}`)
+    const [resultado, setResultado] = useState( )
+    const cargarname = () => {
+        let nombre = document.getElementById('valorName').value;
+        setText(nombre)
+    } 
+    const  datos1 = useService(`https://api.nationalize.io/?name=${text}`)
+    const  datos2 = useService(`https://api.agify.io/?name=${text}`)
+    const  datos3 = useService(`https://api.genderize.io?name=${text}`)
     const handleInput = () => {
-        setResultado({ ...simulacionderequest, nombre: text })
         let resultadofinal = {
+            nombre: datos1.data.name,
             nacionalidades: datos1.data.country,
             edad: datos2.data.age,
             genero: datos3.data.gender
         }
-        let nombre = document.getElementById('valorName').value;
-        setText(nombre)
-        
-        if( simulacionderequest.datos.nacionalidades[0] !== undefined  ){
-            console.log("estoy en el if 1  ", simulacionderequest.datos.nacionalidades)
-            setResultado({ ...simulacionderequest, state: true, nombre: nombre })
-            console.log(simulacionderequest)
-            document.getElementById('valorName').value = "";
-            
+        setResultado(resultadofinal)
+        if( resultadofinal.nacionalidades[0] !== undefined  ){
+            setResultado({ ...resultadofinal, state: true })
+            document.getElementById('valorName').value = ""
         }
-
         else{
-            console.log("estoy en el else ", simulacionderequest.datos.nacionalidades[0])
-            console.log('tambien entro')
-            setResultado({ ...simulacionderequest, state: false}) 
-        }
-        
+            setResultado({ ...resultadofinal, state: false}) 
+        }        
     };
     return (
         <>
         <p className="texto-introduc">En esta aplicación haremos una prediccion lo mas exacta posible de tu edad, genero y las posibles nacionalidades con sus respectivos valores de probabilidad.</p>
-        <input placeholder="Ingresa Tu Nombre..." className="input-text"  id='valorName' ></input>
+        <input placeholder="Ingresa Tu Nombre..." className="input-text" onChange={cargarname} id='valorName' ></input>
         <button className="btnprimario" onClick={handleInput}>Buscar</button>
-        <div> {resultado ? <PrintText  resultado={resultado}  /> : ""}
+        <div> {resultado ? <PrintText  datos={resultado}  /> : ""}
         </div>
     
         </>
@@ -64,15 +40,4 @@ import useService from "../hooks/UseService";
 }
 
 export default Recuadro;
-/*
 
-            {
-                country_id: "Col",
-                probability: 0.002254
-            },
-            {  
-                country_id: "arg",
-                probability: 0.902254
-            } 
-            
-            */
